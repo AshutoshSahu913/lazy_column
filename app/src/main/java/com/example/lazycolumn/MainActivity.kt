434ce4a4573cbeaea6d60d67d9ef137ee9891e32
Ashutosh <1ashutoshsahu@gmail>
2024-03-30 16:37:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/lazycolumn/MainActivity.kt
@@ -0,0 +1,180 @@
+package com.example.lazycolumn
+
+import android.os.Bundle
+import android.provider.CalendarContract.Colors
+import android.widget.AbsListView.RecyclerListener
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FilledIconButton
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lazycolumn.ui.theme.LazyColumnTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LazyColumnTheme {
+                // A surface container using the 'background' color from the theme
+//                RecyclerView()
+                ImageView()
+            }
+        }
+    }
+}
+
+@Composable
+fun RecyclerView(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(4.dp)) {
+        items(items = names) { name ->
+            ListItems(name = name)
+        }
+    }
+}
+
+@Composable
+fun ImageView() {
+
+    Box(
+        modifier = Modifier
+            .padding(10.dp)
+            .clip(CircleShape)
+            .background(Color.Yellow)
+            .size(200.dp),
+        contentAlignment = Alignment.Center,
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.img1),
+            contentDescription = "image",
+            modifier = Modifier.fillMaxHeight(),
+            contentScale = ContentScale.FillHeight
+        )
+    }
+}
+
+
+@Composable
+fun ListItems(name: String) {
+
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded.value) 24.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+
+
+    Surface(
+        color = Color.Cyan,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp, vertical = 4.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(24.dp)
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(
+                        text = "Open to Work", fontStyle = FontStyle.Normal,
+                        fontWeight = FontWeight.Bold
+                    )
+                    Text(
+                        text = name,
+                        style = MaterialTheme.typography.bodyLarge.copy(
+                            fontWeight = FontWeight.ExtraBold
+                        ),
+                        fontSize = 32.sp,
+                        color = Color.White
+                    )
+
+                }
+                ElevatedButton(onClick = { expanded.value = !expanded.value }) {
+                    Text(
+                        if (expanded.value) "Show less" else "Show more",
+                        fontStyle = FontStyle.Normal,
+                        color = Color.Black
+                    )
+                }
+            }
+            if (expanded.value) {
+                Column(
+                    modifier = Modifier.padding(
+                        bottom = extraPadding.coerceAtLeast(0.dp)
+                    )
+                ) {
+                    Text(
+                        text = "Hi,\n" +
+                                "\n" +
+                                "I'm impressed by your expertise in Android development. I'm eager to connect, find advice on app development, exchange insights, and explore potential collaborations. Let's connect on LinkedIn.\n" +
+                                "\n" +
+                                "Regards", fontFamily = FontFamily.SansSerif
+
+                    )
+                    Spacer(modifier = Modifier.padding(top = 10.dp))
+                    Image(
+                        painterResource(id = R.drawable.android),
+                        contentDescription = ""
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LazyColumnTheme {
+        ImageView()
+    }
+}
